e0268acfae1a9e814859828086503de5328a1f48
DmitriyGovosyants <Demiangvs@gmail.com>
2022-05-30 21:17:14 +0300

    features: base functional

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import './sass/main.scss';
+import { fetch } from './js/film-api.js';
+import galleryMarkup from './templates/gallery.hbs';
+
+const refs = {
+    form: document.querySelector('.search'),
+    gallery: document.querySelector('.gallery'),
+}
+
+refs.form.addEventListener('submit', onSearch);
+
+async function onSearch(e) {
+    e.preventDefault();
+    
+    let search = e.currentTarget.elements.query.value.trim();
+    console.log(search); //delete after
+    const { data: {results} } = await fetch(search);
+    clearMarkup();
+    render(results);
+
+    e.target.reset();
+}
+
+function render(results) {
+    refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup(results));
+}
+
+function clearMarkup() {
+    refs.gallery.innerHTML = '';
+}
+
---
+++ src/js/film-api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const API_KEY = 'bfe20768c956c05046c7d088e4b361cd';
+const BASE_URL = 'https://api.themoviedb.org/3/search/movie/';
+
+async function fetch(search) {
+    return await axios.get(`${BASE_URL}?api_key=${API_KEY}&query=${search}&page=1`);
+}
+
+export { fetch };
