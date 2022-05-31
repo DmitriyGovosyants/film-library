279d7d4527e8cd24150da473603116af457a8fe9
DmitriyGovosyants <Demiangvs@gmail.com>
2022-05-31 15:14:28 +0300

    fix: double GET request

--- src/js/film-api.js
+++ src/js/film-api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_KEY = 'bfe20768c956c05046c7d088e4b361cd';
-const BASE_URL = 'https://api.themoviedb.org/3/search/movie/';
+const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
 
 async function fetch(search) {
     return await axios.get(`${BASE_URL}?api_key=${API_KEY}&query=${search}&page=1`);
