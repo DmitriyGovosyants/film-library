69bd7282e5ee44d25e7fcc986b19dbd92eeea91b
DmitriyGovosyants <Demiangvs@gmail.com>
2022-06-01 18:21:19 +0300

    features: add genres name

--- src/index.js
+++ src/index.js
@@ -1,49 +1,75 @@
 import './sass/main.scss';
-import { fetch } from './js/film-api.js';
+import { getFilmByName, getPopularFilm, updateFilmGenre } from './js/film-api.js';
 import galleryMarkup from './templates/gallery.hbs';
 
 const refs = {
-    form: document.querySelector('.search'),
-    gallery: document.querySelector('.gallery'),
+  form: document.querySelector('.search'),
+  gallery: document.querySelector('.gallery'),
 }
 
-refs.form.addEventListener('submit', onSearch);
+document.addEventListener('DOMContentLoaded', onSiteLoad);
+refs.form.addEventListener('submit', onFilmSearch);
 
-async function onSearch(e) {
-    e.preventDefault();
-    let search = e.currentTarget.elements.query.value.trim();
-    if (!search) {
-        return;
-    }
-    
-    const { data: {results} } = await fetch(search);
-    clearMarkup();
-    transformDate(results);
-    render(results);
-
-    e.target.reset();
+async function onSiteLoad() {
+  const { data: { results } } = await getPopularFilm();
+  const { data: { genres } } = await updateFilmGenre();
+  transformDate(results);
+  transformGenre(results, genres);
+  render(results);
 }
 
-document.addEventListener('DOMContentLoaded', async () => {
-    const { data: { results } } = await fetch('iron man');
-    transformDate(results);
-    render(results);
-})
+async function onFilmSearch(e) {
+  e.preventDefault();
+  let search = e.currentTarget.elements.query.value.trim();
+  if (!search) {
+      return;
+  }
+  
+  const { data: { results } } = await getFilmByName(search);
+  const { data: { genres } } = await updateFilmGenre();
+  clearMarkup();
+  transformDate(results);
+  transformGenre(results, genres);
+  render(results);
 
-function render(results) {
-    refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup(results));
+  e.target.reset();
+}
+
+function render(data) {
+  refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup(data));
 }
 
 function clearMarkup() {
-    refs.gallery.innerHTML = '';
+  refs.gallery.innerHTML = '';
 }
 
 function transformDate(data) {
   data.map(item => {
     if (item.release_date) {
       item.release_date = item.release_date.slice(0, 4);
-      }
+    }
       
+    return item;
+  });
+}
+
+function transformGenre(data, genres) {
+  data.map(item => {
+    let newGenre = [];
+
+    if (item.genre_ids) {
+      item.genre_ids.forEach(id => {
+        const rigthID = genres.find(item => item.id === id);
+        newGenre.push(rigthID.name);
+      });
+    }
+    if (newGenre.length >= 4) {
+      const manyGenres = newGenre.slice(0, 3);
+      item.genres = manyGenres.join(', ');
+    } else {
+      item.genres = newGenre.join(', ');
+    }
+
     return item;
   });
 }
--- src/js/film-api.js
+++ src/js/film-api.js
@@ -1,10 +1,22 @@
 import axios from "axios";
 
 const API_KEY = 'bfe20768c956c05046c7d088e4b361cd';
-const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const START_PATH = '/movie/popular';
+const GENRE_PATH = '/genre/movie/list';
+const SEARCH_PATH = '/search/movie';
 
-async function fetch(search) {
-    return await axios.get(`${BASE_URL}?api_key=${API_KEY}&query=${search}&page=1`);
+
+async function getPopularFilm() {
+    return await axios.get(`${BASE_URL}${START_PATH}?api_key=${API_KEY}&page=1`);
 }
 
-export { fetch };
+async function updateFilmGenre() {
+    return await axios.get(`${BASE_URL}${GENRE_PATH}?api_key=${API_KEY}`);
+}
+
+async function getFilmByName(search) {
+    return await axios.get(`${BASE_URL}${SEARCH_PATH}?api_key=${API_KEY}&query=${search}&page=1`);
+}
+
+export { getFilmByName, getPopularFilm, updateFilmGenre };
